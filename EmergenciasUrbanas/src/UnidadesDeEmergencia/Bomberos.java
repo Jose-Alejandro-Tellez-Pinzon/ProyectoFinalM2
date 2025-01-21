aae374a9c8d6363b5092581d6ebd021b613b2565
Juliana Benavides Reales <jbenavides09@misena.edu.co>
2025-01-21 10:39:04 -0500

    Atributos y Metodos

--- EmergenciasUrbanas/src/UnidadesDeEmergencia/Bomberos.java
+++ EmergenciasUrbanas/src/UnidadesDeEmergencia/Bomberos.java
@@ -2,4 +2,52 @@ package UnidadesDeEmergencia;
 
 public class Bomberos {
 
+    private String estado;
+    private String tipoUnidad;
+    private int matricula; 
+    private int capacidadDelTanqueDeAgua;
+    private String equipamiento;
+    private String sistemaDeBombeo;
+    
+    public int getCapacidadDelTanqueDeAgua() {
+        return capacidadDelTanqueDeAgua;
+    }
+
+    public void setCapacidadDelTanqueDeAgua(int capacidadDelTanqueDeAgua) {
+        this.capacidadDelTanqueDeAgua = capacidadDelTanqueDeAgua;
+    }
+
+    public String getEquipamiento() {
+        return equipamiento;
+    }
+
+    public void setEquipamiento(String equipamiento) {
+        this.equipamiento = equipamiento;
+    }
+
+    public String getSistemaDeBombeo() {
+        return sistemaDeBombeo;
+    }
+
+    public void setSistemaDeBombeo(String sistemaDeBombeo) {
+        this.sistemaDeBombeo = sistemaDeBombeo;
+    }
+
+    public Bomberos (String estado, String tipoUnidad, int matricula) {
+        this.estado = estado;
+        this.tipoUnidad = tipoUnidad;
+        this.matricula = matricula;
+    }
+
+    public String responderLlamado() {
+        return "La Unidad " + tipoUnidad + " está respondiendo al llamado.";
+    }
+
+    public String getasignarEmergencia (String unidadDisponible) {
+        return this.estado = unidadDisponible;
+    }
+
+    public int getMatricula () {
+        return this.matricula;
+    }
 }
